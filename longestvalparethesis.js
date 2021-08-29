63c78ff0ac4c305d46e6e7b9243d3e3d74ac3b3b
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-29 12:54:50 -0700

    Update longestvalparethesis.js

--- longestvalparethesis.js
+++ longestvalparethesis.js
@@ -1,6 +1,6 @@
 var longestValidParentheses = function(s) {
         var dp = (function (s) { var a = []; while (s-- > 0)
-                    a.push(0); return a; })(s.length + 1);
+                    a.push(0); return a; })(s.length + 1);  // placing the element at the top of the stack , then returning a
                 var max = 0;
                 for (var i = 1; i < s.length; i++) {
                     {
@@ -19,3 +19,6 @@ var longestValidParentheses = function(s) {
                 }
                 return max;
             };
+
+
+// tc: O(n^2)
