156ddee1bd9ba6c5d81df9b5be7f4fc5a9ec78d9
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-29 21:22:15 -0700

    Update longestvalparethesis.js

--- longestvalparethesis.js
+++ longestvalparethesis.js
@@ -1,8 +1,8 @@
 var longestValidParentheses = function(s) {
-        var dp = (function (s) { var a = []; while (s-- > 0)
+        let dp = (function (s) { let a = []; while (s-- > 0)
                     a.push(0); return a; })(s.length + 1);  // placing the element at the top of the stack 
-                var max = 0;
-                for (var i = 1; i < s.length; i++) {
+                let max = 0;
+                for (let i = 1; i < s.length; i++) {
                     {
                         if ((function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(s.charAt(i)) == ')'.charCodeAt(0) && i - dp[i] >= 1 && (function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(s.charAt(i - dp[i] - 1)) == '('.charCodeAt(0)) {
                             dp[i + 1] = dp[i - dp[i] - 1] + dp[i] + 2;   //checks indices for i if function(c) will moderate in i what elements in the array are iteratable next and this continues during iteratation across the stack
@@ -10,7 +10,7 @@ var longestValidParentheses = function(s) {
                     }
                     ;
                 }
-                for (var i = 0; i < dp.length; i++) {  //another for loop to loop through the array on dp.length to find max value of i
+                for (let i = 0; i < dp.length; i++) {  //another for loop to loop through the array on dp.length to find max value of i
                     {
                         if (dp[i] > max)
                             max = dp[i];
@@ -22,3 +22,7 @@ var longestValidParentheses = function(s) {
 
 
 // tc: O(n^2)
+
+
+
+
